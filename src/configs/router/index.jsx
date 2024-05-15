8e417f505cd5072e383969725b243e9566d037b1
crossxjonathan <crossxjonathan@gmail.com>
2024-05-15 22:53:12 +0700

    static navbar

--- src/configs/router/index.jsx
+++ src/configs/router/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from '../../pages/Landing'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../../pages/login/login';
+import Home from './../../pages/home/home';
+import PageLayout from '../../pages/pageLayout';
+import MainPage from './../../pages/mainPage';
 
 const MainRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route element={<PageLayout/>}>
+                <Route path="/" element={<Home />} />
+                </Route>
+                <Route element={<MainPage/>}>
+                <Route path="/home" element={<Home />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/hoc/public/headerPublic.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import LogoBlanja from '../../assets/logo/Group 1158.png'
+import SearchBar from '../../component/base/search/search';
+import Category from '../../component/base/sort/sort';
+import Cart from '../../component/base/cart/cart';
+import HeaderButton from '../../component/base/button/headerButton';
+
+const headerPublic = (props) => {
+
+  return (
+    <header className='flex justify-between bg-white shadow-md py-7'>
+        <div className='flex px-28 gap-10'>
+            <img className='absolute top-4' src={LogoBlanja} alt="logoblanja" />
+            <div className='flex absolute py-0 px-48 gap-5'>
+            <SearchBar/>
+            <Category className=""/>
+            </div>
+        </div>
+        <div>
+        <Cart
+            classname="relative cursor-pointer left-96 py-2"
+        />
+        </div>
+        <div className='flex pr-24 gap-5'>
+            <HeaderButton
+            name="Login"
+            className="flex justify-center bg-red-maroon"
+            onClick="/login"
+          />
+           <HeaderButton
+            name="Sign up"
+            className="flex justify-center text-gray-300"
+            onClick="/register"
+          />
+        </div>
+    </header>
+  )
+}
+
+export default headerPublic
