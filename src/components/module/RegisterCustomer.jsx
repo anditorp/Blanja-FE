d173b645a5bf4683b94437b783a8f19a2b27a2de
naufandarmawan <naufandarmawan@gmail.com>
2024-05-17 19:26:28 +0700

    integrasi register

--- src/components/module/RegisterCustomer.jsx
+++ src/components/module/RegisterCustomer.jsx
@@ -1,16 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../../components/base/Input'
 import Button from '../../components/base/Button'
+import api from '../../configs/api'
+import { useNavigate } from 'react-router-dom'
+
 
 const RegisterCustomer = () => {
+
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    name: '',
+  })
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    // console.log(form);
+    api.post('/register/customers', {
+      email: form.email,
+      password: form.password,
+      name: form.name,
+    })
+      .then((res) => {
+        console.log(res.response);
+        alert(`Register berhasil dengan email ${form.email} dan password ${form.password}. Silakan Login`)
+        navigate('/login')
+      })
+      .catch((err) => {
+        console.log(err.response);
+        const error = err.response.data
+        alert(`Anda gagal register - ${error.message}`)
+      })
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div className='w-full flex flex-col gap-10'>
       <div className='w-full flex flex-col gap-4'>
-        <Input label='' placeholder='Name'/>
-        <Input label='' placeholder='Email'/>
-        <Input label='' placeholder='Password'/>
+        <Input
+          type='text'
+          value={form.name}
+          onChange={handleChange}
+          name="name"
+          label=""
+          placeholder="Masukkan nama"
+        />
+        <Input
+          type='email'
+          value={form.email}
+          onChange={handleChange}
+          name="email"
+          label=""
+          placeholder="Masukkan email"
+        />
+        <Input
+          type='password'
+          value={form.password}
+          onChange={handleChange}
+          name="password"
+          label=""
+          placeholder="Masukkan password"
+        />
       </div>
-      <Button className='w-full' />
+      <Button className='w-full' onClick={handleRegister} text='Daftar' />
     </div>
   )
 }
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react'
-import Input from '../../components/base/Input'
-import Button from '../../components/base/Button'
 import { Link, useNavigate } from 'react-router-dom'
-import api from '../../configs/api'
 import Logo from '../../assets/logo.svg'
 import RegisterCustomer from '../../components/module/RegisterCustomer'
 import RegisterSeller from '../../components/module/RegisterSeller'
 
-
-
 const Register = () => {
+    
+
     const [toggle, setToggle] = useState(1)
     const handleToggle = (id) => {
         setToggle(id)
@@ -27,7 +24,7 @@ const Register = () => {
                     <div className='flex flex-col gap-10 w-full items-center'>
                         <ul className='flex rounded-md border border-[#9b9b9b] overflow-hidden w-fit items-center'>
                             <li className='flex flex-col gap-[11px] cursor-pointer' onClick={() => handleToggle(1)}>
-                                <div className={toggle === 1 ? 'p-[15px] bg-[#DB3022] font-bold text-base text-center leading-5 text-[#FFFFFF] w-32' : 'p-[15px] bg-white font-bold text-base text-center leading-5 text-[#9b9b9b] w-32'}>Customer</div>
+                                <div className={toggle === 1 ? 'p-[15px] bg-[#DB3022] font-bold text-base text-center leading-5 leading text-[#FFFFFF] w-32' : 'p-[15px] bg-white font-bold text-base text-center leading-5 text-[#9b9b9b] w-32'}>Customer</div>
                             </li>
                             <li className='flex flex-col gap-[11px] cursor-pointer' onClick={() => handleToggle(2)}>
                                 <div className={toggle === 2 ? 'p-[15px] bg-[#DB3022] font-bold text-base text-center leading-5 text-[#FFFFFF] w-32' : 'p-[15px] bg-white font-bold text-base text-center leading-5 text-[#9b9b9b] w-32'}>Seller</div>
@@ -46,78 +43,6 @@ const Register = () => {
                 </div>
             </div>
         </div>
-        // <div className="bg-[#F6F7F8]">
-
-        //     <div className="px-[75px] py-[39px] max-lg:px-[30px]">
-        //         <div className='container mx-auto flex gap-[70px] max-lg:flex-col'>
-
-        //             <div className='flex flex-col basis-1/2'>
-        //                 <HeroAuth>Temukan developer berbakat & terbaik di berbagai bidang keahlian</HeroAuth>
-        //             </div>
-
-        //             <div className='flex flex-col basis-1/2'>
-        //                 <FormContainer formTitle='Halo, Pewpeople' formDescription='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.'>
-        //                     <div className="flex flex-col gap-4">
-        //                         <Input
-        //                             type='text'
-        //                             value={form.name}
-        //                             onChange={handleChange}
-        //                             name="name"
-        //                             label="Name"
-        //                             placeholder="Masukkan nama"
-        //                         />
-        //                         <Input
-        //                             type='email'
-        //                             value={form.email}
-        //                             onChange={handleChange}
-        //                             name="email"
-        //                             label="Email"
-        //                             placeholder="Masukkan email"
-        //                         />
-        //                         <Input
-        //                             type='text'
-        //                             value={form.company}
-        //                             onChange={handleChange}
-        //                             name="company"
-        //                             label="Perusahaan"
-        //                             placeholder="Masukan nama perusahaan"
-        //                         />
-        //                         <Input
-        //                             type='text'
-        //                             value={form.position}
-        //                             onChange={handleChange}
-        //                             name="position"
-        //                             label="Jabatan"
-        //                             placeholder="Posisi di perusahaan Anda"
-        //                         />
-        //                         <Input
-        //                             type='tel'
-        //                             value={form.phone}
-        //                             onChange={handleChange}
-        //                             name="phone"
-        //                             label="Phone"
-        //                             placeholder="Masukkan phone"
-        //                         />
-        //                         <Input
-        //                             type='password'
-        //                             value={form.password}
-        //                             onChange={handleChange}
-        //                             name="password"
-        //                             label="Password"
-        //                             placeholder="Masukkan password"
-        //                         />
-        //                     </div>
-        //                     <div className="flex flex-col gap-4">
-        //                         <Button variant='primary-yellow' onClick={handleRegister} text='Daftar' />
-        //                         <p className="text-center font-normal text-base text-[#1F2A36]">Anda sudah punya akun? <Link className="text-[#FBB017]" to="/login">Masuk disini</Link></p>
-        //                     </div>
-        //                 </FormContainer>
-        //             </div>
-
-        //         </div>
-        //     </div >
-
-        // </div >
     )
 }
 
